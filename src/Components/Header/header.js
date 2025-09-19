42b849bbebeffb331a19d3eb031fb7edb811265c
Gabriel Pinheiro <gabrielpinheiros3.gp@gmail.com>
2025-09-19 12:15:48 -0300

    Correção da  importação não usada que causava erro no Netlify

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "./header.css";
