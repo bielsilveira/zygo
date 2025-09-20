18e5b0b50c75a520551c82e3f0188de31224688b
Gabriel Pinheiro <gabrielpinheiros3.gp@gmail.com>
2025-09-19 21:14:46 -0300

    Correção do google analytics

--- src/Components/analyticsgoogle.js
+++ src/Components/analyticsgoogle.js
@@ -1,18 +1,23 @@
 // src/components/analyticsgoogle.js
-import  { useEffect } from 'react';
+
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const GoogleAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    // Quando a URL mudar, enviar a visualização de página ao Google Analytics
-    window.gtag('config', 'G-MTS365EBS9', {
-      page_path: location.pathname + location.search,
-    });
+    // Verifica se o gtag está disponível antes de chamar
+    if (window.gtag) {
+      window.gtag('config', 'G-MTS365EBS9', {
+        page_path: location.pathname + location.search,
+      });
+    } else {
+      console.warn('Google Analytics não carregou corretamente');
+    }
   }, [location]);
 
-  return null; // Esse componente não precisa renderizar nada
+  return null;
 };
 
 export default GoogleAnalytics;
